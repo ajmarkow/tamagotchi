a9df3abfc460c34699c43bfbfed12ed14378d66d
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-01 10:15:43 -0700

    Pass pre-written test for setting food level for tamagotchi.
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>
    Co-authored by: Forrest Reiland <freiland@email.arizona.edu>

--- lib/Tamagotchis.rb
+++ lib/Tamagotchis.rb
@@ -1,2 +1,25 @@
-class Tamagotchis
+require "rspec"
+
+class Tamagotchi
+  attr_accessor(:food_level, :sleep_level, :name, :activity_level)
+  @@alltamagotchis = {}
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @food_level = attributes.fetch(:food_level)
+    @sleep_level = attributes.fetch(:sleep_level)
+    @activity_level = attributes.fetch(:activity_level)
+  end
+
+  def name
+    @name
+  end
+
+  def is_alive
+    if @food_level > 0 && @sleep_level > 0 && @activity_level > 0
+      return true
+    else
+      return false
+    end
+  end
 end
--- spec/Tamagotchis_spec.rb
+++ spec/Tamagotchis_spec.rb
@@ -1,7 +1,10 @@
+require "Tamagotchis"
+require "rspec"
+
 describe(Tamagotchi) do
   describe("#initialize") do
     it("sets the name and life levels of a new Tamagotchi") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagotchi.new({ :name => "lil dragon", :food_level => 10, :sleep_level => 10, :activity_level => 10 })
       expect(my_pet.name()).to(eq("lil dragon"))
       expect(my_pet.food_level()).to(eq(10))
       expect(my_pet.sleep_level()).to(eq(10))
@@ -11,12 +14,13 @@ describe(Tamagotchi) do
 
   describe("#is_alive") do
     it("is alive if the food level is above 0") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagotchi.new({ :name => "lil dragon", :food_level => 10, :sleep_level => 10, :activity_level => 10 })
+
       expect(my_pet.is_alive()).to(eq(true))
     end
 
     it("is dead if the food level is 0") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagotchi.new({ :name => "lil dragon", :food_level => 10, :sleep_level => 10, :activity_level => 10 })
       my_pet.set_food_level() #make a method that will change the food level of your tamagotchi.
       expect(my_pet.is_alive()).to(eq(false))
     end
@@ -24,7 +28,7 @@ describe(Tamagotchi) do
 
   describe("#time_passes") do
     it("decreases the amount of food the Tamagotchi has left by 1") do
-      my_pet = Tamagotchi.new("lil dragon")
+      my_pet = Tamagotchi.new({ :name => "lil dragon", :food_level => 10, :sleep_level => 10, :activity_level => 10 })
       my_pet.time_passes()  #decide what trigger you will use to make time pass
       expect(my_pet.food_level()).to(eq(9))
     end
